955baca63c6de977943ddd16ded12a7cb97556c9
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-26 13:23:39 +0530

    declared an ArrayList

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer>arr = new ArrayList<>();// 1. declare an ArrayList in Java
+    }
+}
