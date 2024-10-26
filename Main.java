6026a6b8b201f1c3bae08574bace8ab7fa42a88d
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-26 14:11:24 +0530

    continued add() method

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import java.util.*;
+
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer>arr = new ArrayList<>();// 1. declare an ArrayList in Java
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();// 1. declare an ArrayList in Java
         ArrayList<Integer> a1 = new ArrayList<>(5); // declaring an ArrayList with initial capacity
         arr.add(12); // 2. added an element at the end of ArrayList
         arr.add(13);
@@ -15,5 +17,11 @@ public class Main {
         for (Integer elemInteger : arr) {
             System.out.println(elemInteger);
         }
+        a1.add(23);
+
+        for (Integer elemInteger : a1) {
+            System.out.println(elemInteger);
+        }
+        sc.close();
     }
 }
