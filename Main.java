26f3dfd76dc95383880b051d3e4e4ea0beae5f04
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-26 13:42:14 +0530

    done add() method and other related methods

--- Main.java
+++ Main.java
@@ -6,5 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Integer>arr = new ArrayList<>();// 1. declare an ArrayList in Java
+        ArrayList<Integer> a1 = new ArrayList<>(5); // declaring an ArrayList with initial capacity
+        arr.add(12); // 2. added an element at the end of ArrayList
+        arr.add(13);
+        arr.addFirst(7);
+        arr.addLast(90);
+        arr.add(1, 456);
+        for (Integer elemInteger : arr) {
+            System.out.println(elemInteger);
+        }
     }
 }
