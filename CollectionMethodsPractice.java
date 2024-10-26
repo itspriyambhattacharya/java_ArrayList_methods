447b8836686696e15509e92d0852b3ba215c9849
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-26 19:04:21 +0530

    Collections.reverse()

--- CollectionMethodsPractice.java
+++ CollectionMethodsPractice.java
@@ -6,8 +6,16 @@ public class CollectionMethodsPractice {
         ArrayList<Integer> arr = new ArrayList<>();
 
         // Collections.addAll() method
-        Collections.addAll(arr, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Collections.addAll(arr, 1, 262, 3, 34, 51, 46, 7, 58, 9, 10);
         System.out.println("The Array is:\n");
+        System.out.println(arr);
+
+        // Collections.sort()
+        Collections.sort(arr);
+        System.out.println("The sorted list is " + arr);
+
+        // Collections.reverse()
+        Collections.reverse(arr);
         System.out.println(arr);
         sc.close();
     }
